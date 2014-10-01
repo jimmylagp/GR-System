e6bca4712137a8c56c0fd067547920f8dc611631
Luis Antonio Gomez Prieto <lgomez@digizent.com>
2014-09-30 20:54:31 -0500

    Agregar archivos estaticos y nuevos modulos

--- grsystem/urls.py
+++ grsystem/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^', include('sellSystem.urls')),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATICFILES_DIRS}),
+	url(r'^', include('sellSystem.urls')),
 )
